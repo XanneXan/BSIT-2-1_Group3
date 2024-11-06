c3b1cabed60a46c57c83dc0b6d3eab65f8b51215
Sebastian Rafael <Sebastian Rafael@DESKTOP-EFJJ7PS>
2024-11-06 17:44:32 +0800

    "Update for Student Frame"

---
+++ src/studentmanagamentsystem/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentmanagamentsystem;
+
+/**
+ *
+ * @author Sebastian Rafael
+ */
+public class main {
+    public static void main(String[] args) {
+        
+        studentFrame run = new studentFrame();
+        run.setVisible(true);
+        
+    }
+    
+}
